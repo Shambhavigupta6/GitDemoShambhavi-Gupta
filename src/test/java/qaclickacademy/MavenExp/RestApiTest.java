c7c2ba50faea7f08866ddce99351a026effdfccb
Shambhavi <shambhavigupta6@gmail.com>
2021-09-30 21:54:45 +0530

    first commit

---
+++ src/test/java/qaclickacademy/MavenExp/RestApiTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.MavenExp;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+
+	@Test
+	public void postJira() {
+		System.out.println("postJira");
+	}
+
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+}
